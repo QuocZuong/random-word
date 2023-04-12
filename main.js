d5422bdab10823d49d200815977d2c48c1ffa048
Quốc Zương <110612155+QuocZuong@users.noreply.github.com>
2023-04-12 11:21:52 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const fileInput = document.getElementById("fileInput");
 const copyButton = document.getElementById("copyButton");
 const resetButton = document.getElementById("resetButton");
 const inputWord = document.getElementById("inputWord");
+const darkMode = document.getElementById("darkModeBtn");
+const scrollToTop = document.getElementById("upToHeader");
+
 inputNumber.addEventListener("input", function(event) {
     let afterFilter = inputNumber.value.replace(/[^0-9]/g, ""); //remove character word in text
     if (afterFilter !== "") {
@@ -14,7 +17,7 @@ inputNumber.addEventListener("input", function(event) {
     inputNumber.cols += Math.ceil(inputNumber.value.length / 10);
 });
 
-inputNumber.addEventListener("keydown", function(event) {
+inputNumber.addEventListener("keydown", function(event) { //function to calculate length of textarea input number of word when user delete character
     const previousLength = inputNumber.value.length;
 
     setTimeout(function() {
@@ -22,25 +25,25 @@ inputNumber.addEventListener("keydown", function(event) {
         if (currentLength === 0) {
             inputNumber.cols = 1;
         } else if (currentLength < previousLength) {
-            inputNumber.cols -= 1 + Math.ceil((previousLength - currentLength) / 10);
+            inputNumber.cols -= 1 + Math.ceil((previousLength - currentLength) / 10); // Math.ceil round up to nearest int number. Ex: (10 - 6) / 10 = 0.4 round to 1
         }
     }, 0);
 });
 
-copyButton.addEventListener("click", () => {
+copyButton.addEventListener("click", () => { // function for copy button
     const textToCopy = quoteDisplay.innerHTML;
     navigator.clipboard.writeText(textToCopy);
 });
 
-generateButton.addEventListener("click", () => {
+generateButton.addEventListener("click", () => { // function generate random word
     inputWord.style.display = "none";
     let numWords = inputNumber.value;
     if (numWords === "") {
-        numWords = 1000;
+        numWords = 100; // set default equal 100
     }
 
-    if (fileInput.files.length) {
-        const file = fileInput.files[0];
+    if (fileInput.files.length) { // generate by file input
+        const file = fileInput.files[0]; // create file contain object
         const fileReader = new FileReader();
         fileReader.readAsText(file);
         fileReader.onload = () => {
@@ -49,14 +52,14 @@ generateButton.addEventListener("click", () => {
 
             let randomWords = "";
             for (let i = 0; i < numWords; i++) {
-                const randomIndex = Math.floor(Math.random() * words.length);
+                const randomIndex = Math.floor(Math.random() * words.length); // Math.floor round down
                 const word = words[randomIndex];
                 randomWords += `${word} `;
             }
 
             quoteDisplay.innerHTML = randomWords.trim();
         };
-    } else if (inputWord.value.length) {
+    } else if (inputWord.value.length) { // generate by user input word
         const words = inputWord.value.split(" ");
         let randomWords = "";
         for (let i = 0; i < numWords; i++) {
@@ -65,12 +68,12 @@ generateButton.addEventListener("click", () => {
             randomWords += `${word} `;
         }
         quoteDisplay.innerHTML = randomWords.trim();
-    } else {
-        fetch("/assets/text.txt")
+    } else { // generate by default txt file of server
+        fetch("/assets/text.txt") // using fetch API to get content of local file text.txt
             .then((response) => response.blob())
-            .then((blob) => {
+            .then((blob) => { // content of file are read as a blob object 
                 const fileReader = new FileReader();
-                fileReader.readAsText(blob);
+                fileReader.readAsText(blob); // fileReader.readAsText need a blob object
                 fileReader.onload = () => {
                     const fileContent = fileReader.result;
                     const words = fileContent.split(" ");
@@ -90,4 +93,20 @@ generateButton.addEventListener("click", () => {
 
 resetButton.addEventListener("click", () => {
     location.reload();
+});
+
+darkMode.addEventListener("click", () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+window.addEventListener("scroll", function() {
+    if (this.window.pageYOffset > 0) {
+        scrollToTop.style.display = 'block';
+    } else {
+        scrollToTop.style.display = 'none';
+    }
+})
+
+scrollToTop.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
 });
